627e94264aa78a17de45739963eff7bc95e98dc8
Rodolfo Caputo <rodcap.93@gmail.com>
2015-07-01 22:28:57 -0300

    Aumento de experiencia aprolijado

--- src/tadp_pokemon/Pokemon.scala
+++ src/tadp_pokemon/Pokemon.scala
@@ -8,57 +8,46 @@ case class Pokemon(
  
   def nivel(unNivel: Int): Pokemon = copy(nivel = unNivel)
 
-  def experiencia(unaExperiencia: Int): Pokemon = copy(experiencia = unaExperiencia)
-
-  def genero(unGenero: Genero): Pokemon = copy(genero = unGenero)
-
   def energia(unaEnergia: Int): Pokemon = copy(energia = unaEnergia)
-
-  def energiaMaxima(unaEnergiaMaxima: Int): Pokemon = copy(energiaMaxima = unaEnergiaMaxima)
- 
-  def peso(unPeso: Int): Pokemon = copy(peso = unPeso)
-
-  def fuerza(unaFuerza: Int): Pokemon = copy(fuerza = unaFuerza)
-
-  def velocidad(unaVelocidad: Int): Pokemon = copy(velocidad = unaVelocidad)
-
+  
   def estado(unEstado: Estado): Pokemon = copy(estado = unEstado)
 
-  def especie(unaEspecie: Especie): Pokemon = copy(especie = unaEspecie)
+  def aumentarEnergiaMaxima(unaEnergiaMaxima: Int): Pokemon = copy(energiaMaxima = energiaMaxima+unaEnergiaMaxima)
+ 
+  def aumentarFuerza(unaFuerza: Int): Pokemon = copy(fuerza = fuerza + unaFuerza)
   
-  def aumentarEnergia(unaCantidad: Int): Pokemon = energia(unaCantidad.min(energiaMaxima))
+  def aumentarVelocidad(unaVelocidad: Int): Pokemon = copy(velocidad = velocidad + unaVelocidad)
+  
+  def aumentarPeso(unPeso: Int): Pokemon = copy(peso = (peso + unPeso).min(especie.pesoMaximo))
+  
+  def aumentarEnergia(unaCantidad: Int): Pokemon = energia((energia+unaCantidad).min(energiaMaxima))
 
   def ataques(unosAtaques: List[Ataque]): Pokemon = {
     this.copy(ataques = unosAtaques.filter{ataque => ataque.tipo == this.especie.tipoPrincipal })
   }
 
-  def aprenderAtaque(unAtaque: Ataque): Pokemon = if(especie.esAfin(unAtaque)) copy(ataques = ataques:+unAtaque) else this
+  def aprenderAtaque(unAtaque: Ataque): Pokemon = 
+    if(especie.esAfin(unAtaque)) copy(ataques = ataques:+unAtaque) else this
 
+  def aumentarExperiencia(unaCantidadExperiencia: Int) = 
+    copy(experiencia = experiencia + unaCantidadExperiencia).subirNivel(unaCantidadExperiencia)
+    
+  def subirNivel(unaCantidadExperiencia: Int) =
+      if (puedeSubirNivelSegunExperiencia(unaCantidadExperiencia)) aumentarCaracteristicas() else this
 
-  def aumentarExperiencia(unaCantidadExperiencia: Int) {
-    if (puedeSubirNivelSegunExperiencia(unaCantidadExperiencia)) {
-      this.subirNivel().evolucionar().experiencia(experiencia + unaCantidadExperiencia)
-    } else {
-      this.experiencia(experiencia + unaCantidadExperiencia);
-    }
-  }
-
-  def puedeSubirNivelSegunExperiencia(unaCantidadExperiencia: Int): Boolean = {
+  def puedeSubirNivelSegunExperiencia(unaCantidadExperiencia: Int): Boolean = 
     unaCantidadExperiencia >= experienciaNecesariaProximoNivel(nivel)
-  }
 
-  def experienciaNecesariaProximoNivel(unNivel: Int): Int = {
-    if (nivel >= 1) (2 * experienciaNecesariaProximoNivel(nivel - 1)) + especie.resistenciaEvolutiva
-    else 0
-  }
+  def experienciaNecesariaProximoNivel(unNivel: Int): Int = 
+    if (nivel >= 1) (2 * experienciaNecesariaProximoNivel(nivel - 1)) + especie.resistenciaEvolutiva else 0
 
-  def subirNivel(): Pokemon = {
-    this.energiaMaxima(energiaMaxima + especie.incrementoEnergiaMaxima).peso(peso + especie.incrementoPeso).fuerza(fuerza + especie.incrementoFuerza).velocidad(velocidad + especie.incrementoVelocidad)
-  }
+  def aumentarCaracteristicas(): Pokemon = 
+    aumentarEnergiaMaxima(especie.incrementoEnergiaMaxima)
+    .aumentarPeso(especie.incrementoPeso)
+    .aumentarFuerza(especie.incrementoFuerza)
+    .aumentarVelocidad(especie.incrementoVelocidad)
 
-  def aumentarEnergiaAlMaximo() {
-    this.energia(energiaMaxima)
-  }
+  def aumentarEnergiaAlMaximo() = energia(energiaMaxima)
 
   def evolucionar(): Pokemon = {
     if (especie.criterioEvolucion.criterio(this)) {
