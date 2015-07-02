727a6d9e7562597e6bd341c1569ff4f5b868c4a4
ricardo cascallares <ricardo_cascallares@bnetbuilders.com>
2015-07-02 17:45:30 -0300

    agrego algunos test al ataque(estan asumiendo cosas del dominio que seguramente haya que cambiar)

--- test/tadp_pokemon/AtaqueTest.scala
+++ test/tadp_pokemon/AtaqueTest.scala
@@ -49,20 +49,64 @@ class AtaqueTest {
   
   @Test
   def `atacar decrementa la cantidad de puntos de ataque` = {
-    val especiePikachu = new Especie
-    especiePikachu.tipoPrincipal(Electrico)
+    val especiePikachu = new Especie().tipoPrincipal(Electrico)
     
     val rayito = new Ataque(Electrico, 10)
     val atacarConRayito = new RealizarAtaque()
     atacarConRayito.ataque = rayito
     
-    var pikachu = new Pokemon
-    pikachu.especie(especiePikachu)
-    pikachu.aprenderAtaque(rayito)
+    var pikachu = new Pokemon().especie(especiePikachu).aprenderAtaque(rayito)
     
     pikachu = atacarConRayito.realizarActividad(pikachu)
     assertEquals((rayito, 9, 10), pikachu.ataques.head)
     pikachu = atacarConRayito.realizarActividad(pikachu)
     assertEquals((rayito, 8, 10), pikachu.ataques.head)
   }
+
+
+def `atacar aumenta sus puntos de experiencia` = {
+    val especiePikachu = new Especie().tipoPrincipal(Electrico).experiencia(100)
+    
+    val rayito = new Ataque(Electrico, 10)
+    val atacarConRayito = new RealizarAtaque()
+    atacarConRayito.ataque = rayito
+    
+    var pikachu = new Pokemon().especie(especiePikachu).aprenderAtaque(rayito)
+    
+    pikachu = atacarConRayito.realizarActividad(pikachu)
+  
+    assertEquals(pikachu.experiencia,150);
+   
+  }
+
+def `atacar con ataque del tipo secundario y es macho aumenta sus puntos de experiencia en 20 ` = {
+    val especiePikachu = new Especie().genero(Macho).tipoPrincipal(volador).experiencia(100).tipoSecundario(Electrico)
+    
+    val rayito = new Ataque(Electrico, 10)
+    val atacarConRayito = new RealizarAtaque()
+    atacarConRayito.ataque = rayito
+    
+    var pikachu = new Pokemon().especie(especiePikachu).aprenderAtaque(rayito)
+    
+    pikachu = atacarConRayito.realizarActividad(pikachu)
+  
+    assertEquals(pikachu.experiencia,120);
+   
+  }
+
+def `atacar con ataque del tipo secundario y es hembra aumenta sus puntos de experiencia en 40` = {
+    val especiePikachu = new Especie().genero(Hembra).tipoPrincipal(volador).experiencia(100).tipoSecundario(Electrico)
+    
+    val rayito = new Ataque(Electrico, 10)
+    val atacarConRayito = new RealizarAtaque()
+    atacarConRayito.ataque = rayito
+    
+    var pikachu = new Pokemon().especie(especiePikachu).aprenderAtaque(rayito)
+    
+    pikachu = atacarConRayito.realizarActividad(pikachu)
+  
+    assertEquals(pikachu.experiencia,140);
+   
+  }
+
 }
