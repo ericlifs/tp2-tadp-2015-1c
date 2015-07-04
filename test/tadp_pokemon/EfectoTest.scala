c688af95a39344ed860143f9d36ea6be99ebb59d
norchow <nicoorchow@gmail.com>
2015-07-04 03:41:25 -0300

    Fixes tests anteriores de efecto y pokemon

--- test/tadp_pokemon/EfectoTest.scala
+++ test/tadp_pokemon/EfectoTest.scala
@@ -1,26 +1,26 @@
 package tadp_pokemon
 
+import scala.util.{Try,Success,Failure}
+
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.Ignore
 
 class EfectoTest {
-  /*
+  
+   var pikachu:Pokemon
+  
   @Test
   def `el efecto reposar sube la energía al máximo y lo deja dormido` = {
-    val especiePikachu = new Especie
-    especiePikachu.tipoPrincipal = Electrico
+    val especiePikachu = new Especie(tipoPrincipal=Electrico,criterioEvolucion=new CriterioSubirNivel(100),pesoMaximo= 100,resistenciaEvolutiva=100).tipoSecundario(Volador)
     
-    val rayito = new Ataque(Electrico, 10, Reposar)
-    val atacarConRayito = new RealizarAtaque()
-    atacarConRayito.ataque = rayito
+    val rayito = new Ataque(new AtaqueBase(Electrico,10,null))
+    val atacarConRayito = new RealizarAtaque(new AtaqueBase(Electrico,30,null))
     
-    var pikachu = new Pokemon
-    pikachu.especie(especiePikachu)
-    pikachu.energiaMaxima(100)
-    pikachu.aprenderAtaque(rayito)
+    pikachu = Pokemon(genero=Macho,especie=Especie(tipoPrincipal= Electrico ,criterioEvolucion=new CriterioSubirNivel(100),pesoMaximo= 100,resistenciaEvolutiva=100))
+    pikachu.aprenderAtaque(new AtaqueBase(Electrico,10,null))
     
-    pikachu = atacarConRayito.realizarActividad(pikachu)
+    val trypikachu: Try[Pokemon] =  atacarConRayito.realizarActividad(Try(pikachu))
     
     assertEquals(100, pikachu.energia)
     assertEquals(Dormido, pikachu.estado)
@@ -28,41 +28,32 @@ class EfectoTest {
   
   @Test
   def `el efecto enfocarse sube la velocidad un punto` = {
-    val especiePikachu = new Especie
-    especiePikachu.tipoPrincipal = Electrico
+    val especiePikachu = new Especie(tipoPrincipal=Electrico,criterioEvolucion=new CriterioSubirNivel(100),pesoMaximo= 100,resistenciaEvolutiva=100).tipoSecundario(Volador)
     
-    val rayito = new Ataque(Electrico, 10, Enfocarse)
-    val atacarConRayito = new RealizarAtaque()
-    atacarConRayito.ataque = rayito
+    val rayito = new Ataque(new AtaqueBase(Electrico,10,null))
+    val atacarConRayito = new RealizarAtaque(new AtaqueBase(Electrico,30,null))
     
-    var pikachu = new Pokemon
-    pikachu.especie(especiePikachu)
-    pikachu.velocidad(10)
-    pikachu.aprenderAtaque(rayito)
-    
-    val mew = new Pokemon
-    pikachu = atacarConRayito.realizarActividad(pikachu)
+    pikachu = Pokemon(genero=Macho,especie=Especie(tipoPrincipal= Electrico ,criterioEvolucion=new CriterioSubirNivel(100),pesoMaximo= 100,resistenciaEvolutiva=100))
+    pikachu.aprenderAtaque(new AtaqueBase(Electrico,10,null))
+
+    val trypikachu: Try[Pokemon] =  atacarConRayito.realizarActividad(Try(pikachu))
     
     assertEquals(11, pikachu.velocidad)
   }
   
   @Test
   def `el efecto endurecerse sube la energía en 5 puntos y lo paraliza` = {
-    val especiePikachu = new Especie
-    especiePikachu.tipoPrincipal = Electrico
+    val especiePikachu = new Especie(tipoPrincipal=Electrico,criterioEvolucion=new CriterioSubirNivel(100),pesoMaximo= 100,resistenciaEvolutiva=100).tipoSecundario(Volador)
     
-    val rayito = new Ataque(Electrico, 10, Endurecerse)
-    val atacarConRayito = new RealizarAtaque()
-    atacarConRayito.ataque = rayito
+    val rayito = new Ataque(new AtaqueBase(Electrico,10,null))
+    val atacarConRayito = new RealizarAtaque(new AtaqueBase(Electrico,30,null))
     
-    var pikachu = new Pokemon
-    pikachu.especie(especiePikachu)
-    pikachu.energia(100)
-    pikachu.aprenderAtaque(rayito)
+    pikachu = Pokemon(genero=Macho,especie=Especie(tipoPrincipal= Electrico ,criterioEvolucion=new CriterioSubirNivel(100),pesoMaximo= 100,resistenciaEvolutiva=100))
+    pikachu.aprenderAtaque(new AtaqueBase(Electrico,10,null))
     
-    pikachu = atacarConRayito.realizarActividad(pikachu)
+    val trypikachu: Try[Pokemon] =  atacarConRayito.realizarActividad(Try(pikachu))
     
     assertEquals(105, pikachu.energia)
     assertEquals(Paralizado, pikachu.estado)
-  }*/
+  }
 }
--- test/tadp_pokemon/PokemonTest.scala
+++ test/tadp_pokemon/PokemonTest.scala
@@ -4,14 +4,17 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.Ignore
 
-class PokemonTest {/*
+class PokemonTest {
+  
+  var pikachu:Pokemon
+  
   @Test
   def `respeta energia maxima` = {
-    val pikachu = new Pokemon
+    pikachu = Pokemon(genero=Macho,especie=Especie(tipoPrincipal= Electrico ,criterioEvolucion=new CriterioSubirNivel(100),pesoMaximo= 100,resistenciaEvolutiva=100))
 
     pikachu.aumentarEnergia(20)
     assertEquals(120, pikachu.energia)
     pikachu.aumentarEnergia(100)
     assertEquals(150, pikachu.energia)
-  }*/
+  }
 }
