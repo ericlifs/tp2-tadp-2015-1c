f8611e2d91d77e59bfff13f39b01601aae993673
Rodolfo Caputo <rodcap.93@gmail.com>
2015-07-06 02:37:36 -0300

    Agrego un fold con composicion que me habia olvidado y queda más lindo :P

--- src/tadp_pokemon/Actividad.scala
+++ src/tadp_pokemon/Actividad.scala
@@ -6,6 +6,8 @@ trait Actividad {
   
   def siPuede(pokemon: Try[Pokemon]): Try[Pokemon] = pokemon
   
+  val actividad: Try[Pokemon] => Try[Pokemon] = realizarActividad(_)
+  
   def realizarActividad(pokemon: Try[Pokemon]): Try[Pokemon] =
     pokemon.flatMap(_.hacerActividad(this)).map(_.efectosPosterioresActividad(this))
     
--- src/tadp_pokemon/Rutina.scala
+++ src/tadp_pokemon/Rutina.scala
@@ -7,17 +7,13 @@ trait Rutina{
 }
 
 case class RutinaComun(val actividades: List[Actividad] = List(), val nombre: String = "No me pusieron nombre :(") extends Rutina{
-    
-  def realizar(pokemon: Pokemon): Try[Pokemon] =
-    actividades.foldLeft[Try[Pokemon]](Success(pokemon)){
-    (pokemon:Try[Pokemon], actividad: Actividad) =>
-      actividad.realizarActividad(pokemon)    
-    }
   
+  def realizar(pokemon: Pokemon): Try[Pokemon] ={
+    actividades.map(_.actividad).foldLeft(identity[Try[Pokemon]] _)(_ compose _) (Success(pokemon))
+  }
 }
-
-case object RutinaVacia extends Rutina{
+case object RutinaVacia extends Rutina{                                                                                                                                                                                                                                                                                                                                          
   
     def realizar(pokemon: Pokemon): Try[Pokemon] = Success(pokemon)
   
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
